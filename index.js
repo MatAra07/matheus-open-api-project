fff19a10323067ab48b0c52a6c81633f0525cb49
MatAra07 <mat.araujo.business@gmail.com>
2026-02-20 13:19:21 -0500

    API Fetch completed

---
+++ index.js
@@ -0,0 +1,10 @@
+fetch("API_URL_HERE")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
