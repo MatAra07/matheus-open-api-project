9468f6c324b3ad58dc19fe50247c871870b1041f
MatAra07 <mat.araujo.business@gmail.com>
2026-02-21 11:56:53 -0500

    API fetch completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("API_URL_HERE")
+fetch(https://api.open-meteo.com/v1/forecast?latitude=40.7128&longitude=-74.0060&current_weather=true
   .then(function(response) {
     return response.json();
   })
